96aacd38f869348aa6fc766c020bea3f0ace5741
Ivan Shirykhalov <ivanshirykhalov@gmail.com>
2022-01-12 21:09:53 +0300

    'components'

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,65 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
 }
 
-export default App;
+export function AppTitle() {
+    return (
+        <>APP component</>
+    )
+}
+
+export default App
+
+export function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+export function Star() {
+    return (
+        <div>star</div>
+    )
+}
+
+export function Accordion() {
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+export function AccordionTitle() {
+    return (
+        <h3>Menu</h3>
+    )
+}
+
+export function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
+
